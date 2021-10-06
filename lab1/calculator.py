68318aedbe91aeda0b2ebc9233495ce2a551baf5
Riccardo Gallo <riccardogallo@MBP-di-Riccardo.homenet.telecomitalia.it>
2021-10-06 12:44:48 +0200

    Added the butraction, multiply and gcd operation. Also inserted several test of all the operations

--- lab1/calculator.py
+++ lab1/calculator.py
@@ -15,6 +15,36 @@ def sum(m, n):
     else:
         raise Exception
 
+def subtract(m, n):
+    if type(m) is int and type(n) is int:
+        result = m
+        if(n > 0):
+            while(n > 0):
+                result -= 1
+                n -= 1
+        else:
+            while(n < 0):
+                result += 1
+                n += 1
+        return result
+    else:
+        raise Exception
+
+def multiply(m, n):
+    if type(m) is int and type(n) is int:
+        result = 0
+        if(m == 0 or n == 0):
+            return result
+        positive_result = m > 0 and n > 0 or m < 0 and n < 0
+        m = abs(m)
+        n = abs(n)
+        while(n > 0):
+            result += m
+            n -= 1
+        result = result if positive_result else -result
+        return result
+    else:
+        raise Exception
 
 def divide(m, n):
     if type(m) is int and type(n) is int:
@@ -34,6 +64,19 @@ def divide(m, n):
                 m = m - n
             result = result if positive_result else result
         return result           # the result of the division is in Z
+    else:
+        raise Exception
+
+def gcd(m, n):  #5, 0
+    if type(m) is int and type(n) is int:
+        if(m == 0 and n != 0):
+            return abs(n)
+        elif(m == 0 and n == 0):
+            return 0
+        elif(m != 0 and n == 0):
+            return abs(m)
+        else:
+            return gcd(n, m%n)
     else:
         raise Exception
 
--- lab1/lab1.py
+++ lab1/lab1.py
@@ -8,9 +8,36 @@ class Calculator:
     def sum(self, m, n):
         return c.sum(m, n)
 
+    def subtract(slef, m, n):
+        return c.subtract(m, n)
+
+    def multiply(self, m, n):
+        return c.multiply(m, n)
+
     def divide(self, m, n):
         return c.divide(m, n)
 
-instance = Calculator()
-print("sum:" + str(instance.sum(1, -3)))
+    def gcd(self, m, n):
+        return c.gcd(m, n)
 
+instance = Calculator()
+
+try:
+    print("\nSum1: " + str(instance.sum(270, 192)))
+    print("Sum2: " + str(instance.sum(270, -192)))
+    #print("Sum3_error: " + str(instance.sum(27.5, -192))) # identico in tutte le operazioni.
+    print("Subtraction1: " + str(instance.subtract(270, 192)))
+    print("Subtraction1: " + str(instance.subtract(-270, 192)))
+    print("Multiply1: " + str(instance.multiply(7, 2)))
+    print("Multiply2: " + str(instance.multiply(7, -2)))
+    print("Multiply3: " + str(instance.multiply(-7, -2)))
+    print("Divide1: " + str(instance.divide(7, 2)))
+    print("Divide2: " + str(instance.divide(0, 2)))
+    print("Divide3: " + str(instance.divide(7, 0)))
+    print("Divide4: " + str(instance.divide(0, 0)))
+    print("GCD1: " + str(instance.gcd(270, -192)))
+    print("GCD2: " + str(instance.gcd(0, 192)))
+    print("GCD3: " + str(instance.gcd(270, 0)))
+    print("GCD4: " + str(instance.gcd(0, 0)) + "\n")
+except Exception:
+    print("\nErrore!!\n")
